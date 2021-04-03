829180c264b7f31eaf1750ade5a9216f9923a4a5
BartoszMiz <bartmiz.2004@gmail.com>
2021-04-03 17:58:55 +0200

    Add the Todo item display and fix the date format in the json file

--- TodoApp.Web/Pages/Index.cshtml.cs
+++ TodoApp.Web/Pages/Index.cshtml.cs
@@ -1,25 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TodoApp.Web.Models;
+using TodoApp.Web.Services;
 
 namespace TodoApp.Web.Pages
 {
-    public class IndexModel : PageModel
+	public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+		private readonly ITodoItemProvider todoItemProvider;
+		public IEnumerable<TodoItem> TodoItems { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
+        public IndexModel(ILogger<IndexModel> logger, ITodoItemProvider todoItemProvider)
+		{
             _logger = logger;
-        }
+			this.todoItemProvider = todoItemProvider;
+		}
 
         public void OnGet()
         {
-
+			TodoItems = todoItemProvider.GetTodoItems();
         }
     }
 }
