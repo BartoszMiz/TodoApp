e1e4be92567b00bdd338880461b3b16539d2de41
BartoszMiz <bartmiz.2004@gmail.com>
2021-04-08 13:14:50 +0200

    Add DbContext and edit TodoItem model

--- TodoApp.Web.Core/Models/TodoItem.cs
+++ TodoApp.Web.Core/Models/TodoItem.cs
@@ -1,16 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace TodoApp.Web.Core.Models
 {
 	public class TodoItem
 	{
+		[Key]
 		public int Id { get; set; }
+		[Required]
 		public string Instruction { get; set; }
-		[JsonPropertyName("addition-date")]
+		[JsonPropertyName("addition-date")] [Required]
 		public DateTime AdditionDate { get; set; }
+		[Required]
 		public DateTime Deadline { get; set; }
-		[JsonPropertyName("is-completed")]
+		[JsonPropertyName("is-completed")] [Required]
 		public bool IsCompleted { get; set; }
 	}
 }
